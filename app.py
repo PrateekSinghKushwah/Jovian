034eed91aa145241e24bfc6cc6b7388671282956
PrateekSinghKushwah <63105099+PrateekSinghKushwah@users.noreply.github.com>
2023-10-13 18:34:16 +0000

    Made the page dynamic

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello, World"
+JOBS = [
+    {
+        'id' : 1,
+        'Title' : 'Data Engineer',
+        'Location' : 'Remote',
+        'Salary' : '$120,000'
+    },
+    {
+        'id' : 2,
+        'Title' : 'Cloud Engineer',
+        'Location' : 'Remote',
+        'Salary' : '$120,000'
+    },
+    {
+        'id' : 3,
+        'Title' : 'Software Engineer',
+        'Location' : 'Remote',
+        
+    },
+    {
+        'id' : 4,
+        'Title' : 'Cloud Consultant',
+        'Location' : 'Remote',
+        'Salary' : '$120,000'
+    },
+    {
+        'id' : 5,
+        'Title' : 'Business Analyst',
+        'Location' : 'Remote',
+        'Salary' : '$120,000'
+    },
+]
 
-print(__name__)
+@app.route('/')
+def hello():
+  return render_template('home.html', jobs = JOBS, company_name = 'Getraise Technologies')
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
+
 if __name__ == "__main__":
-  print("I am inside if")
   app.run(host = '0.0.0.0' ,debug=True)
